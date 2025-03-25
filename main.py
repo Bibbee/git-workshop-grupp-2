bceeb0be5f9ca5baa2d881fb0713722b3db34edb
Antonhuitfeldt <anton.huitfeldt@hotmail.com>
2025-03-25 14:59:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-name = input("What is your name?")
-print (name)
+import ibbe
+import sara
+import mickus
+import anton
+
+def main():
+    namn = input("Ange namn: ")
+    if namn == "ibbe":
+        ibbe.hello()
+    elif namn == "mickus":
+        mickus.hello()
+    elif namn == "anton":
+        anton.hello()
+    else:
+        print("Okänt namn!")
+
+main()
