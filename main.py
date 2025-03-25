89335270bed2b66080b52c2bae264f3ff82731b3
sarabenic <Benicsara@gmail.com>
2025-03-25 15:17:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,25 @@ import anton
 
 def main():
     namn = input("Ange namn: ")
+    food = input ("Ange mat")
     if namn == "ibbe":
         ibbe.hello()
     elif namn == "mickus":
         mickus.hello()
     elif namn == "anton":
         anton.hello()
+    elif food == "anton":
+        anton.food()
+    elif food == "mickus":
+        mickus.food()
+    elif food == "ibbe":
+        ibbe.food()
+    elif food == "sara":
+        sara.food()
+    
     else:
         print("Okänt namn!")
 
 main()
+
+
