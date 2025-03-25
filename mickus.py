cc3ac1d3e51b6f2e04c04dc206d4e8033f69aec2
Mickuspetren <petrenmickus@gmail.com>
2025-03-25 14:55:03 +0100

    Update mickus.py

--- mickus.py
+++ mickus.py
@@ -1,2 +1,5 @@
-name = input("“Hi, I’m Mickus")
-print (name)
+def hello():
+    name = input("Hi, I’m Mickus. What's your name? ")  # Fixad input-sträng
+    print("Hello, " + name + "!")  # Skriver ut hälsning
+
+hello()  # Anropar funktionen
